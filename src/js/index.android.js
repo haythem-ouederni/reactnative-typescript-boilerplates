3fd48082576d171dd9833d4198ed6c6f1d53f326
haythem ouederni <haythem.ouederni@gmail.com>
2017-05-04 23:48:36 +0200

    adding native-base library : https://docs.nativebase.io/Components.html#Components

--- src/js/index.android.js
+++ src/js/index.android.js
@@ -4,13 +4,26 @@
  * @flow
  */
 
+'use strict';
+
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry} from 'react-native';
+import {Text, Button, View} from 'native-base';
+
+var pressBro = () => {
+    alert('tourout');
+}
 
 export default class RNTestProject extends Component {
+
+    // props = { }; props = {     title : "bobo",     button : {         title :
+    // 'Pressez moi'     } };
+
     render() {
+
         return (
             <View style={styles.container}>
+
                 <Text style={styles.welcome}>
                     Welcome to React Native !
                 </Text>
@@ -20,14 +33,21 @@ export default class RNTestProject extends Component {
 
                 <Text style={styles.instructions}>
                     Double tap R on your keyboard to reload, {'\n'}
-                    Shake or press menu button for dev menu
+                    Shake or press menu button for dev menu sos
                 </Text>
+                <View style={styles.buttonContainer}>
+                    <Button style={styles.button} onPress={pressBro}>
+                        <Text>Press me</Text>
+                    </Button>
+                </View>
+
             </View>
+
         );
     }
 }
 
-const styles = StyleSheet.create({
+const styles = {
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -43,7 +63,24 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: '#333333',
         marginBottom: 5
+    },
+    buttonContainer: {
+        alignItems: 'center'
+    },
+    button: {
+        marginTop: 35,
+        backgroundColor: '#333333',
+        shadowColor: 'blue',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        // width: 100, height: 60,
+        shadowRadius: 50,
+        shadowOpacity: 0.8,
+        borderRadius: 50
+
     }
-});
+};
 
 AppRegistry.registerComponent('RNTestProject', () => RNTestProject);
