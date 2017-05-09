e1ab2e355f741ed2671559f0f3bf93941cbe3924
haythem ouederni <haythem.ouederni@gmail.com>
2017-05-09 23:52:18 +0200

    First tab implementation of the react-native-navigation library

--- android/app/src/main/java/com/rntestproject/MainApplication.java
+++ android/app/src/main/java/com/rntestproject/MainApplication.java
@@ -8,40 +8,56 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactnativenavigation.NavigationApplication;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  //   @Override
+  //   public boolean getUseDeveloperSupport() {
+  //     return BuildConfig.DEBUG;
+  //   }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
+  //   @Override
+  //   protected List<ReactPackage> getPackages() {
+  //     return Arrays.<ReactPackage>asList(
+  //         new MainReactPackage()
+  //     );
+  //   }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "src/index.android";
-    }
+  //   @Override
+  //   protected String getJSMainModuleName() {
+  //     return "src/index.android";
+  //   }
 
-  };
+  // };
 
+  // @Override
+  // public ReactNativeHost getReactNativeHost() {
+  //   return mReactNativeHost;
+  // }
+
+  // @Override
+  // public void onCreate() {
+  //   super.onCreate();
+  //   SoLoader.init(this, /* native exopackage */ false);
+  // }
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public String getJSMainModuleName() {
+    return "src/index.android";
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return Arrays.<ReactPackage>asList();
   }
 }
--- src/index.android.tsx
+++ src/index.android.tsx
@@ -7,17 +7,18 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {AppRegistry, ViewStyle, TextStyle} from 'react-native';
+import {ViewStyle, TextStyle} from 'react-native';
 import {Text, Button, View} from 'native-base';
+import {Navigation} from 'react-native-navigation';
 
 var pressBro = () => {
     alert('tourout');
 };
 
-interface Props { }
-interface State { }
+interface Props {}
+interface State {}
 
-export default class RNTestProject extends Component<Props, State> {
+export default class RNTestProject extends Component < Props, State > {
 
     // props = { }; props = {     title : "bobo",     button : {         title :
     // 'Pressez moi'     } };
@@ -28,7 +29,7 @@ export default class RNTestProject extends Component<Props, State> {
             <View style={styles.container}>
 
                 <Text style={styles.welcome}>
-                    Welcome to React Native test bro!
+                    Welcome to React Native !
                 </Text>
                 <Text style={styles.instructions}>
                     To get started yes, edit index.android.js
@@ -41,6 +42,35 @@ export default class RNTestProject extends Component<Props, State> {
                 <View style={styles.buttonContainer}>
                     <Button style={styles.button} onPress={pressBro}>
                         <Text>Press me</Text>
+                    </Button>
+                </View>
+
+            </View>
+
+        );
+    }
+}
+
+export class RNTestProject2 extends Component <Props,State> {
+
+    // props = { }; props = {     title : "bobo",     button : {         title :
+    // 'Pressez moi'     } };
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+
+                <Text style={styles.welcome}>
+                    Second tab bro
+                </Text>
+                <Text style={styles.instructions}>
+                    android
+                </Text>
+
+                <View style={styles.buttonContainer}>
+                    <Button style={styles.button} onPress={pressBro}>
+                        <Text>Fuck me</Text>
                     </Button>
                 </View>
 
@@ -86,4 +116,35 @@ const styles = {
     }
 };
 
-AppRegistry.registerComponent('RNTestProject', () => RNTestProject);
+Navigation.registerComponent('react-native-navigation-bootstrap', () => RNTestProject);
+Navigation.registerComponent('react-native-navigation-bootstrap2', () => RNTestProject2);
+// Navigation.startSingleScreenApp({   screen: {     screen:
+// 'react-native-navigation-bootstrap',     title: 'Navigation Bootstrap'   }
+// });
+Navigation.startTabBasedApp({
+    tabs: [
+        {
+            label: 'One',
+            screen: 'react-native-navigation-bootstrap',
+            icon: require('./assets/img/1-freepik.jpg'),
+            selectedIcon: require('./assets/img/1-freepik.jpg'),
+            title: 'Screen One'
+        }, {
+            label: 'two',
+            screen: 'react-native-navigation-bootstrap2',
+            icon: require('./assets/img/1-freepik.jpg'),
+            selectedIcon: require('./assets/img/1-freepik.jpg'),
+            title: 'Screen two'
+        }
+    ],
+    tabsStyle: { // optional, add this if you want to style the tab bar beyond the defaults
+        tabBarButtonColor: '#ffff00', // optional, change the color of the tab icons and text (also unselected)
+        tabBarSelectedButtonColor: '#ff9900', // optional, change the color of the selected tab icon and text (only selected)
+        tabBarBackgroundColor: '#551A8B' // optional, change the background color of the tab bar
+    },
+    appStyle: {
+        orientation: 'portrait' // Sets a specific orientation to the entire app. Default: 'auto'. Supported values: 'auto', 'landscape', 'portrait'
+    },
+    passProps: {}, // simple serializable object that will pass as props to all top screens (optional)
+    animationType: 'slide-down'
+});
