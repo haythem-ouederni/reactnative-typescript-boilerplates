1556fbfafac3f8743a10576c437dfb98f2c9bf65
haythem ouederni <haythem.ouederni@gmail.com>
2017-05-22 00:34:40 +0200

    Make the redux example work for android

--- src/screens/tabs/home/home.android.tsx
+++ src/screens/tabs/home/home.android.tsx
@@ -5,15 +5,12 @@
 
 import React from 'react';
 import {Text, Button, View} from 'native-base';
+import {ScrollView} from 'react-native';
 import {homeStyles as styles} from './home.styles.android';
-import {HomeCommun} from './home-commun';
+import {HomeCommun, mapStateToProps, mapDispatchToProps} from './home-commun';
+import {connect} from 'react-redux';
 
-export class Home extends HomeCommun {
-
-    constructor(props) {
-        super(props);
-
-    }
+class Home extends HomeCommun {
 
     render() {
 
@@ -32,6 +29,11 @@ export class Home extends HomeCommun {
                     Shake or press menu button for dev menu sos
                 </Text>
                 <View style={styles.buttonContainer}>
+                    <Button style={styles.button} onPress={this.retrieveMoviesList}>
+                        <Text>Fetch</Text>
+                    </Button>
+                </View>
+                <View style={styles.buttonContainer}>
                     <Button style={styles.button} onPress={this.pressBro}>
                         <Text>Press me</Text>
                     </Button>
@@ -42,8 +44,29 @@ export class Home extends HomeCommun {
                     </Button>
                 </View>
 
+                <ScrollView
+                    style={{
+                    flex: 0.8,
+                    marginTop: 20
+                }}>
+                    {this
+                        .movies()
+                        .map((movie) => {
+                            return (
+                                <View key={movie._id}>
+                                    <Text
+                                        style={{
+                                        height: 20
+                                    }}>{movie._title}</Text>
+                                </View>
+                            )
+                        })}
+                </ScrollView>
+
             </View>
 
         );
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
