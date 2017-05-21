3218c91dfd9303dfdca6831d79716b7bd42fe8f5
haythem ouederni <haythem.ouederni@gmail.com>
2017-05-22 00:34:26 +0200

    Make the use of react-native-config available for android

--- android/app/src/main/java/com/rntestproject/MainApplication.java
+++ android/app/src/main/java/com/rntestproject/MainApplication.java
@@ -1,13 +1,7 @@
 package com.rntestproject;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
-
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
@@ -15,18 +9,18 @@ import java.util.List;
 
 public class MainApplication extends NavigationApplication {
 
-  @Override
-  public String getJSMainModuleName() {
-    return "build/src/index.android";
-  }
+    @Override
+    public String getJSMainModuleName() {
+        return "build/src/index.android";
+    }
 
-  @Override
-  public boolean isDebug() {
-    return BuildConfig.DEBUG;
-  }
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
 
-  @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-    return Arrays.<ReactPackage>asList();
-  }
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return Arrays.<ReactPackage>asList(new ReactNativeConfigPackage());
+    }
 }
